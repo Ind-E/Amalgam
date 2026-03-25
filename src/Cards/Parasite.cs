30f6dd457197156b7addd692bfb604c85e3bb9f4
Indi Esneault <indiesneault@gmail.com>
2026-03-25 16:20:10 -0500

    impl parasitic shrymp relic (needs relic art)

---
+++ src/Cards/Parasite.cs
@@ -0,0 +1,34 @@
+using BaseLib.Abstracts;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.CardPools;
+
+namespace Amalgam.Cards;
+
+[Pool(typeof(CurseCardPool))]
+public class Parasite() : CustomCardModel(-1, CardType.Curse, CardRarity.Curse, TargetType.None)
+{
+    public override bool CanBeGeneratedByModifiers => false;
+    public override int MaxUpgradeLevel => 0;
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Unplayable];
+    public override IEnumerable<DynamicVar> CanonicalVars => [new MaxHpVar(3)];
+
+    public override async Task BeforeCardRemoved(CardModel card)
+    {
+        if (card != this)
+        {
+            return;
+        }
+        await CreatureCmd.LoseMaxHp(
+            new ThrowingPlayerChoiceContext(),
+            Owner.Creature,
+            DynamicVars.MaxHp.BaseValue,
+            isFromCard: false
+        );
+    }
+}
--- src/Relics/ParasiticShrymp.cs
+++ src/Relics/ParasiticShrymp.cs
@@ -1,6 +1,13 @@
+using Amalgam.Cards;
 using BaseLib.Abstracts;
 using BaseLib.Utils;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Enchantments;
 using MegaCrit.Sts2.Core.Models.RelicPools;
 
 namespace Amalgam.Relics;
@@ -9,4 +16,51 @@ namespace Amalgam.Relics;
 public class ParasiticShrymp : CustomRelicModel
 {
     public override RelicRarity Rarity => RelicRarity.Ancient;
+
+    public override bool HasUponPickupEffect => true;
+
+    public override IEnumerable<IHoverTip> ExtraHoverTips =>
+        HoverTipFactory.FromEnchantment<Imbued>().Append(HoverTipFactory.FromCard<Parasite>());
+
+    private static bool ImbuedAllTypes;
+
+    public override async Task AfterObtained()
+    {
+        CardSelectorPrefs prefs = new(CardSelectorPrefs.EnchantSelectionPrompt, 1);
+        Imbued canonicalImbued = ModelDb.Enchantment<Imbued>();
+
+        ImbuedAllTypes = true;
+        foreach (
+            CardModel item in await CardSelectCmd.FromDeckForEnchantment(
+                Owner,
+                canonicalImbued,
+                1,
+                prefs
+            )
+        )
+        {
+            CardCmd.Enchant(canonicalImbued.ToMutable(), item, 1m);
+            CardCmd.Preview(item);
+        }
+        ImbuedAllTypes = false;
+
+        await CardPileCmd.AddCursesToDeck(
+            [ModelDb.Card<Parasite>(), ModelDb.Card<Parasite>()],
+            Owner
+        );
+    }
+
+    [HarmonyPatch(typeof(Imbued), nameof(Imbued.CanEnchantCardType))]
+    static class ImbuedAllCardTypesPatch
+    {
+        public static bool Prefix(ref bool __result)
+        {
+            if (ImbuedAllTypes)
+            {
+                __result = true;
+                return false;
+            }
+            return true;
+        }
+    }
 }
