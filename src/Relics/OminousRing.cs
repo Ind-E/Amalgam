dbb6ea47ca45d9f5b8f63e16a32ebae06bf98b57
Indi Esneault <indiesneault@gmail.com>
2026-03-26 09:58:38 -0500

    impl ominous ring relic (needs relic art)

--- src/Relics/OminousRing.cs
+++ src/Relics/OminousRing.cs
@@ -1,7 +1,12 @@
 using BaseLib.Abstracts;
 using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Map;
 using MegaCrit.Sts2.Core.Models.RelicPools;
+using MegaCrit.Sts2.Core.Runs;
+using MegaCrit.Sts2.Core.Saves.Runs;
 
 namespace Amalgam.Relics;
 
@@ -9,4 +14,45 @@ namespace Amalgam.Relics;
 public class OminousRing : CustomRelicModel
 {
     public override RelicRarity Rarity => RelicRarity.Ancient;
+
+    public override bool HasUponPickupEffect => true;
+
+    public override IEnumerable<DynamicVar> CanonicalVars => [new GoldVar(666)];
+
+    private int _actPickedUp;
+
+    [SavedProperty]
+    public int ActPickedUp
+    {
+        get { return _actPickedUp; }
+        set
+        {
+            AssertMutable();
+            _actPickedUp = value;
+        }
+    }
+
+    public override async Task AfterObtained()
+    {
+        ActPickedUp = Owner.RunState.CurrentActIndex;
+        await PlayerCmd.GainGold(DynamicVars.Gold.BaseValue, Owner);
+        await RunManager.Instance.GenerateMap();
+    }
+
+    public override ActMap ModifyGeneratedMap(IRunState runState, ActMap map, int actIndex)
+    {
+        if (ActPickedUp != actIndex)
+        {
+            return map;
+        }
+        foreach (var point in map.GetAllMapPoints())
+        {
+            if (point.PointType == MapPointType.Shop)
+            {
+                point.PointType = MapPointType.Unknown;
+            }
+        }
+
+        return map;
+    }
 }
